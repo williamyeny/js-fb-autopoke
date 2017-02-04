d4244eace8b6d10f89558e9e2f09a376b21a0924
williamyeny <williamyeny@gmail.com>
2017-02-04 03:37:53 -0500

    added comments

--- autopoke.js
+++ autopoke.js
@@ -3,11 +3,14 @@
 
     For other versions, replace "Poke Back" by the label used in your version
 */
+console.info("Auto-poke loaded.");
 setInterval(function() {
   links = document.getElementsByTagName("a");
   for (var i = 0 ; i != links.length; i++)
   {
-      if (links[i].innerHTML.contains("Poke Back"))
-          links[i].click();
+    if (links[i].innerHTML.contains("Poke Back")) {
+        links[i].click();
+        console.info("Poking...");
+    }
   }
-}, 1000);
+}, 3000);
