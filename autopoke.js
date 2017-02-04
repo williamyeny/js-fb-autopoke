baa6f5debedc3d7dbf960881c0e726ab77b471c2
williamyeny <williamyeny@gmail.com>
2017-02-04 03:43:53 -0500

    modular loop

--- autopoke.js
+++ autopoke.js
@@ -3,7 +3,10 @@
 
     For other versions, replace "Poke Back" by the label used in your version
 */
-console.info("Auto-poke loaded.");
+console.info("Auto-poke loaded."); //how fast do you want it to check for pokes?
+
+var loopTime = 5000;
+
 setInterval(function() {
   links = document.getElementsByTagName("a");
   for (var i = 0 ; i != links.length; i++)
@@ -13,4 +16,5 @@ setInterval(function() {
         console.info("Poking...");
     }
   }
-}, 3000);
+  console.info("Pausing for " + loopTime + " milliseconds...");
+}, loopTime);
